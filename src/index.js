305d987ef65d5e7dcddf4813203f254cf4468a6d
apid01 <apurvadhakre281@gmail.com>
2024-08-13 15:50:36 +0530

    Added utils and a few generic functions

--- src/index.js
+++ src/index.js
@@ -1,4 +1,16 @@
 import "dotenv/config";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
-connectDB();
+connectDB()
+.then(() => {
+    const port = process.env.PORT || 6000;
+    app.on("error", (error) => {
+        console.log("ERROR: ", error);
+    });
+    app.listen(port);
+    console.log(`Server is running at port : ${port}`);
+})
+.catch((err) => {
+    console.log("MONGO DB connection failed!", err);
+});
